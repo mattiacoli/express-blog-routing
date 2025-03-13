9098cc59f7b2c25a3d7faa7ccdcdd63ce057a17d
mattiacoli <mattia.coli@aol.com>
2025-03-13 15:40:05 +0100

    add middleware for posts endpoint and fix bug in CRUD URI

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const port = 3000
 // import rotuers module
 const postsRouter = require('./routers/posts')
 
+// middleware for posts routers
+app.use('/posts', postsRouter)
+
 // basic route
 app.get('/', (req, res)=>{
   res.send('Welcome on this server')
--- routers/posts.js
+++ routers/posts.js
@@ -2,35 +2,35 @@ const express = require('express')
 const router = express.Router()
 
 // index
-router.get('/posts', (req,res)=>{
+router.get('/', (req,res)=>{
   res.send('Post List here')
 })
 
 //show
-router.get('/posts/:id', (req, res)=>{
+router.get('/:id', (req, res)=>{
   const postId = req.params.id
   res.send(`this is post with id: ${postId}` )
 })
 
 // sort
-router.post('/posts', (req, res)=>{
+router.post('/', (req, res)=>{
   res.send('Add new post')
 })
 
 // Update
-router.put('/posts/:id', (req, res)=>{
+router.put('/:id', (req, res)=>{
   const postId = req.params.id
   res.send(`update entire post with id: ${postId}` )
 })
 
 // modify
-router.patch('/posts/:id', (req, res)=>{
+router.patch('/:id', (req, res)=>{
   const postId = req.params.id
   res.send(`modify part of post with id: ${postId}` )
 })
 
 // delete
-router.delete('/posts/:id', (req, res)=>{
+router.delete('/:id', (req, res)=>{
   const postId = req.params.id
   res.send(`modify part of post with id: ${postId}` )
 })
